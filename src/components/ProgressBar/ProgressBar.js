9820ce7206e09be8fcd4a4b78249c91174981019
Jouse Then <jthen93@gmail.com>
2022-08-18 18:25:09 -0400

    Clean up

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,45 +7,56 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = {
   small: {
-    "--height": "8px",
-    "--borderRadius": "4px",
+    "height": "8px",
+    "borderRadius": "4px",
+    "padding": "0px"
   },
   medium: {
-    "--height": "12px",
-    "--borderRadius": "4px",
+    "height": "12px",
+    "borderRadius": "4px",
+    "padding": "0px"
   },
   large: {
-    "--height": "32px",
-    "--borderRadius": "8px",
-    "--padding": "4px"
+    "height": "32px",
+    "borderRadius": "8px",
+    "padding": "4px"
   }
 };
 const ProgressBar = ({ value, size }) => {
   const styles = SIZES[size];
+
   return( 
-    <Bar style={styles}>
+    <Bar  
+      role="progressbar" 
+      aria-valuenow={value}
+      style={{
+        '--padding': styles.padding ,
+        '--borderRadius': styles.borderRadius ,
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      
       <Progress 
-        role="progressbar" 
-        aria-valuenow={value} 
-        value={value}
-        style={styles}>
-          <VisuallyHidden>{value}%</VisuallyHidden>
-      </Progress>
+      style={{
+            '--width': value + '%',
+            '--height': styles.height,
+            '--borderRadius': value > 98 ? "4px" : "4px 0 0 4px"
+          }}/>
     </Bar>
   );
 };
 const Bar = styled.div`
 box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 border-radius: var(--borderRadius);
-background-color: ${COLORS.transparentGray15}
+background-color: ${COLORS.transparentGray15};
+padding: var(--padding);
 `;
 
 const Progress = styled.div`
 height: var(--height);
-padding: var(--padding);
-border-radius: ${props => props.value > 98 ? `var(--borderRadius)` : `var(--borderRadius) 0 0 var(--borderRadius)`};
 background-color: ${COLORS.primary};
-width: ${props => props.value}%
+width: var(--width);
+border-radius: var(--borderRadius);
 `;
 
 
